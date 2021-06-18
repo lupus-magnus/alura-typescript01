9273822a23f983a7916cc09b44bbf325cd4a37bd
Lobo <matheuscardoso1@poli.ufrj.br>
2021-06-18 16:01:13 -0300

    learned about generics, defensive programming, how to attribute types to arrays and how to simplify the creation of new classes. That's the end of part 1

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,9 @@
+import { NegociacaoController } from "./controllers/necociacao-controller.js";
 import { Negociacao } from "./models/negociacao.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.volume);
+const controller = new NegociacaoController();
+const form = document.querySelector(".form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  controller.adiciona();
+});
